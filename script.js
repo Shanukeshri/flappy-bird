b0c1d28f98f6636316b0174765c6a1263ee90a59
Shanu Keshri <shanukeshri983@gmail.com>
2025-02-02 18:54:25 +0530

    Moving pillars

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-let alive = true;
-
-function play() {
-    alive = true;
-    while (alive) {
-        createpillar();
-    }
-}
-
 function createpillar() {
 
+  let multi = Math.random;
+
     setInterval(() => {
-
-        let pillarPair = document.createElement("div");
-        pillarPair.classList.add("pillar-unit")
-        pillarPair.style.left = `1300px`
-
-        document.body.appendChild(pillarPair)
-
-        let pillarUp = document.createElement("div");
-        pillarUp.classList.add("pillar-up")
-        pillarPair.appendChild(pillarUp);
-        
-        let pillarDown = document.createElement("div");
-        pillarDown.classList.add("pillar-down")
-        pillarPair.appendChild(pillarDown);
-
-        let cnt = 1
-        let rt=2.5
-        while(pillarPair.style.left >= `-90px`){
-            requestAnimationFrame(()=>{
-
-                pillarPair.style.left = `${1300 - 2.5 * cnt }px`
-                cnt++
-
-            })
+      let pillarPair = document.createElement("div");
+      pillarPair.classList.add("pillar-unit");
+      pillarPair.style.left = "1300px";
+      document.body.appendChild(pillarPair);
+  
+      let pillarUp = document.createElement("div");
+      pillarUp.classList.add("pillar-up");
+      
+      pillarPair.appendChild(pillarUp);
+  
+      let pillarDown = document.createElement("div");
+      pillarDown.classList.add("pillar-down");
+      
+      pillarPair.appendChild(pillarDown);
+  
+      let lt = 1300;
+  
+      function movePillar() {
+        if (lt >= -90) {
+          pillarPair.style.left = `${lt - 2.5}px`; 
+          lt -= 2.5;
+          requestAnimationFrame(movePillar);
+        } else {
+          pillarPair.remove();
         }
-        pillarPair.remove();
-
-    }, 1750)
-
+      }
+  
+      requestAnimationFrame(movePillar);
+    }, 1750);
 }
-
-function jump() {
-
-
-}
-
-createpillar();
+  
+  createpillar();
